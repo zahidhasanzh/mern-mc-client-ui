8cdafe8f7c707d31ee5818a430b637196e73e4fd
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-07-07 10:52:08 +0600

    add manrope font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Manrope} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${manrope.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to pizza shop</h1>
+      <h1 >Welcome to pizza shop</h1>
       <Button>Click</Button>
     </main>
   );
