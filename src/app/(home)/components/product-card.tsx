adf10f10aa7eab66beda5b31f4512c3c8a7c4fde
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-07-08 12:06:31 +0600

    render available option dynamicaly

--- src/app/(home)/components/product-card.tsx
+++ src/app/(home)/components/product-card.tsx
@@ -6,23 +6,10 @@ import {
   CardFooter,
   CardHeader,
 } from "@/components/ui/card";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Label } from "@/components/ui/label";
-import ToppingList from "./topping-list";
-import { ShoppingCart } from "lucide-react";
-import { Button } from "@/components/ui/button";
+
 import { Product } from "@/lib/types";
-
-// export type Product = {
-//   id: string;
-//   name: string;
-//   description: string;
-//   image: string;
-//   price: number;
-// };
-
+import ProductModel from "./product-model";
 type PropTypes = { product: Product };
 
 const ProductCard = ({ product }: PropTypes) => {
@@ -46,132 +33,7 @@ const ProductCard = ({ product }: PropTypes) => {
           <span className="font-bold ml-1">${50}</span>
         </p>
 
-        <Dialog>
-          <DialogTrigger className="bg-orange-200 hover:bg-orange-300 text-orange-500 px-6 py-2 rounded-full shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">
-            {" "}
-            Choose
-          </DialogTrigger>
-          <DialogContent className="w-full !max-w-3xl  p-0 ">
-            <div className="flex">
-              <div className="w-1/3 bg-white rounded p-8 flex items-center justify-center">
-                <Image
-                  src={product.image}
-                  width={450}
-                  height={450}
-                  alt={product.name}
-                />
-              </div>
-              <div className="2/3 p-8">
-                <h3 className="text-xl font-bold">{product.name}</h3>
-                <p className="mt-1">{product.description}</p>
-
-                <div>
-                  <h4 className="mt-6">Choose the size</h4>
-                  <RadioGroup
-                    defaultValue="small"
-                    className="grid grid-cols-3 gap-3 mt-2"
-                  >
-                    <div>
-                      <RadioGroupItem
-                        value="small"
-                        id="small"
-                        className="peer sr-only"
-                        aria-label="Small"
-                      />
-
-                      <Label
-                        htmlFor="small"
-                        className="flex flex-col items-center justify-between rounded-md border-2  bg-white p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                      >
-                        Small
-                      </Label>
-                    </div>
-                    <div>
-                      <RadioGroupItem
-                        value="medium"
-                        id="medium"
-                        className="peer sr-only"
-                        aria-label="Medium"
-                      />
-
-                      <Label
-                        htmlFor="medium"
-                        className="flex flex-col items-center justify-between rounded-md border-2 bg-white p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                      >
-                        Medium
-                      </Label>
-                    </div>
-
-                    <div>
-                      <RadioGroupItem
-                        value="large"
-                        id="large"
-                        className="peer sr-only"
-                        aria-label="Large"
-                      />
-
-                      <Label
-                        htmlFor="large"
-                        className="flex flex-col items-center justify-between rounded-md border-2  bg-white p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                      >
-                        Large
-                      </Label>
-                    </div>
-                  </RadioGroup>
-                </div>
-
-                <div>
-                  <h4 className="mt-6">Choose the crust</h4>
-                  <RadioGroup
-                    defaultValue="thin"
-                    className="grid grid-cols-3 gap-3 mt-2"
-                  >
-                    <div>
-                      <RadioGroupItem
-                        value="thin"
-                        id="thin"
-                        className="peer sr-only"
-                        aria-label="Thin"
-                      />
-
-                      <Label
-                        htmlFor="thin"
-                        className="flex flex-col items-center justify-between rounded-md border-2  bg-white p-3 hover:bg-accent hover:text-accent-foreground-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                      >
-                        Thin
-                      </Label>
-                    </div>
-                    <div>
-                      <RadioGroupItem
-                        value="thick"
-                        id="thick"
-                        className="peer sr-only"
-                        aria-label="Thick"
-                      />
-
-                      <Label
-                        htmlFor="thick"
-                        className="flex flex-col items-center justify-between rounded-md border-2 bg-white p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                      >
-                        Thick
-                      </Label>
-                    </div>
-                  </RadioGroup>
-                </div>
-
-                <ToppingList />
-
-                <div className="flex items-center justify-between mt-12">
-                  <span className="font-bold">$80</span>
-                  <Button>
-                    <ShoppingCart size={20}/>
-                    <span className="ml-2">Add to cart</span>
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </DialogContent>
-        </Dialog>
+       <ProductModel product={product}/>
       </CardFooter>
     </Card>
   );
---
+++ src/app/(home)/components/product-model.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Image from "next/image";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { ShoppingCart } from "lucide-react";
+import ToppingList from "./topping-list";
+import { Product } from "@/lib/types";
+
+const ProductModel = ({ product }: { product: Product }) => {
+  return (
+    <Dialog>
+      <DialogTrigger className="bg-orange-200 hover:bg-orange-300 text-orange-500 px-6 py-2 rounded-full shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">
+        {" "}
+        Choose
+      </DialogTrigger>
+      <DialogContent className="w-full !max-w-3xl  p-0 ">
+        <div className="flex">
+          <div className="w-1/3 bg-white rounded p-8 flex items-center justify-center">
+            <Image
+              src={product.image}
+              width={450}
+              height={450}
+              alt={product.name}
+            />
+          </div>
+          <div className="2/3 p-8">
+            <h3 className="text-xl font-bold">{product.name}</h3>
+            <p className="mt-1">{product.description}</p>
+
+            {Object.entries(product.category.priceConfiguration).map(
+              ([key, value]) => (
+                <div key={key}>
+                  <h4 className="mt-6">Choose the {key}</h4>
+                  <RadioGroup
+                    defaultValue={value.availableOptions[0]}
+                    className="grid grid-cols-3 gap-3 mt-2"
+                  >
+                    {value.availableOptions.map((option) => (
+                      <div key={option}>
+                        <RadioGroupItem
+                          value={option}
+                          id={option}
+                          className="peer sr-only"
+                          aria-label={option}
+                        />
+
+                        <Label
+                          htmlFor={option}
+                          className="flex flex-col items-center justify-between rounded-md border-2  bg-white p-3 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                        >
+                         {option}
+                        </Label>
+                      </div>
+                    ))}
+
+                  </RadioGroup>
+                </div>
+              )
+            )}
+
+            <ToppingList />
+
+            <div className="flex items-center justify-between mt-12">
+              <span className="font-bold">$80</span>
+              <Button>
+                <ShoppingCart size={20} />
+                <span className="ml-2">Add to cart</span>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ProductModel;
